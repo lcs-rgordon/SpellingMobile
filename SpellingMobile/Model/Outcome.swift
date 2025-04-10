a077be919d8786eac222dbe6317a85b573b457e2
Russell Gordon <rgordon@lcs.on.ca>
2025-04-10 07:47:04 -0400

    Added an enumeration with three cases (possible values) to track an outcome of a guess in the game.

---
+++ SpellingMobile/Model/Outcome.swift
@@ -0,0 +1,16 @@
+//
+//  Outcome.swift
+//  SpellingMobile
+//
+//  Created by Russell Gordon on 2025-04-10.
+//
+
+import Foundation
+
+// Tracks what is happening
+// with a given guess
+enum Outcome: String {
+    case undetermined = " "
+    case correct = "✅"
+    case incorrect = "❌"
+}
