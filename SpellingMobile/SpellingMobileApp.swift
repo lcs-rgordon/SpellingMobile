9f0770b9a3c093d9be03023fba249402dad361aa
Russell Gordon <rgordon@lcs.on.ca>
2025-04-10 07:43:08 -0400

    Initial Commit

---
+++ SpellingMobile/SpellingMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingMobileApp.swift
+//  SpellingMobile
+//
+//  Created by Russell Gordon on 2025-04-10.
+//
+
+import SwiftUI
+
+@main
+struct SpellingMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
