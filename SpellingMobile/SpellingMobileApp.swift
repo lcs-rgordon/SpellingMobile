102eaacf2a109d445d53696ee7b7ac2bed8ee5f2
Russell Gordon <rgordon@lcs.on.ca>
2025-04-10 07:44:39 -0400

    Initial project organization (converted blue folders to grey groups, added folders for MVVM).

--- SpellingMobile/SpellingMobileApp.swift
+++ SpellingMobile/SpellingMobileApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SpellingMobileApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QuizView()
         }
     }
 }
--- SpellingMobile/Views/QuizView.swift
+++ SpellingMobile/Views/QuizView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  QuizView.swift
 //  SpellingMobile
 //
 //  Created by Russell Gordon on 2025-04-10.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct QuizView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    QuizView()
 }
